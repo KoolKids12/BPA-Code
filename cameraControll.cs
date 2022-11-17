94eca19404f10528fff4d594441ae9e33ac803df
KoolKids12 <118480685+KoolKids12@users.noreply.github.com>
2022-11-17 15:59:57 -0500

    Add files via upload
    Change, inverted.

---
+++ cameraControll.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControll : MonoBehaviour
+{
+    [SerializeField]
+    private float _mouseSensitivity = 3.0f;
+
+    private float _rotationY;
+    private float _rotationX;
+
+    [SerializeField]
+    private Transform _target;
+
+    [SerializeField]
+    private float _distanceFromTarget = 3.0f;
+
+    private Vector3 _currentRotation;
+    private Vector3 _smoothVelocity = Vector3.zero;
+
+    [SerializeField]
+    private float _smoothTime = 0.2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity;
+
+        _rotationY += mouseX;
+        _rotationX += mouseY;
+
+        _rotationX = Mathf.Clamp(_rotationX, -40, 40);
+        _rotationY = Mathf.Clamp(_rotationY, 40, -40);
+
+        Vector3 nextRotation = new Vector3(_rotationX, _rotationY);
+        _currentRotation = Vector3.SmoothDamp(_currentRotation, nextRotation, ref _smoothVelocity, _smoothTime);
+        transform.localEulerAngles = _currentRotation;
+
+        transform.position = _target.position - transform.forward * _distanceFromTarget;
+    }
+}
