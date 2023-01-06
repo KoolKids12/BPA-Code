816e41d8c54dedc869a6088e3d9637eb66b293b1
KoolKids12 <118480685+KoolKids12@users.noreply.github.com>
2023-01-06 09:32:31 -0500

    Create Bullet.cs

---
+++ Bullet/Bullet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+   private void OnCollisionEnter2D(Collision2D collision) 
+{
+    Destroy(gameObject);
+
+    if(collision.gameObject.TryGetComponent<Enemy>(out Enemy enemyComponent))
+    {
+        enemyComponent.TakeDamage(1);
+    }
+}
+    
+}
+ 
