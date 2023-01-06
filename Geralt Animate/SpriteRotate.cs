fe7fbbe8489b491943f3a3bfe1e9ecb23ea0157e
KoolKids12 <118480685+KoolKids12@users.noreply.github.com>
2023-01-06 09:40:07 -0500

    Create SpriteRotate.cs

---
+++ Geralt Animate/SpriteRotate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRotate : MonoBehaviour
+{
+
+  void Update () 
+  {
+
+
+    transform.localRotation = transform.localRotation * Quaternion.Inverse(transform.rotation);
+  }
+}
